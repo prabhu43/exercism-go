d0509136081ff74ae0f266957b45f0d9a1a99b70
Prabhu Jayakumar <j.prabhu91@gmail.com>
2018-05-05 14:46:26 +0530

    Add proper comments

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,14 +1,9 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package gigasecond is a library to manipulate time with gigasecond
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond adds a gigasecond to the given time
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(1000000000 * time.Second)
 }
